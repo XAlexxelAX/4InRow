1aab789c70b2e6d6957863a7e9274bbeaaf15dd4
XAlexxelAX <58328844+XAlexxelAX@users.noreply.github.com>
2021-08-02 19:47:20 +0300

    db

---
+++ EFDB/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDB.Models
+{
+    public class Game
+    {
+        public List<User> Players { get; set; } = new List<User>();
+        public int WinnerIndex { get; set; }
+        public DateTime FinishTime { get; set; }
+    }
+}
---
+++ EFDB/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDB.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PW { get; set; }
+        public string Email { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int Score { get; set; }
+    }
+}
