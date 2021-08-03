59029a630dcc741167e66f9dd7442b9330c60025
ShaharBlank <Shahabl@ac.sce.ac.il>
2021-08-03 14:47:44 +0300

    update animation

--- connectFour/MainWindow.xaml.cs
+++ connectFour/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,7 +25,6 @@ namespace connectFour
         private int[,] board;
         private int turn, p1_cellCount, p2_cellCount, p1_score, p2_score;
         private List<Image> imgs;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -48,8 +48,10 @@ namespace connectFour
 
         private void OnPreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+            //  boardView.IsHitTestVisibleProperty = false;
             /*if (e.ClickCount == 2) // for double-click, remove this condition if only want single click
             {*/
+
             var point = Mouse.GetPosition(boardView); // mouse ptr cordiante at click event time
 
             int row = 0, col = 0;
@@ -77,8 +79,15 @@ namespace connectFour
             if (emptyCell_row == -1)
                 return;
 
-            updateBoard(emptyCell_row, col - 2);
+            updateBoard(emptyCell_row, col - 2); // update 2D board + make animation of sliding ball
 
+            // row and col now correspond Grid's RowDefinition and ColumnDefinition mouse was 
+            // over when double clicked!
+            //}
+        }
+
+        private void codeAfterAnimation()
+        {
             p1_cellCount = turn == 0 ? p1_cellCount + 1 : p1_cellCount;
             p2_cellCount = turn == 1 ? p2_cellCount + 1 : p2_cellCount;
             turn = 1 - turn;
@@ -86,25 +95,18 @@ namespace connectFour
             int winnerOrTie = checkForWinnerOrTie();
             if (winnerOrTie != 0) // val!=0 => winner or tie
             {
-                MessageBoxResult result = MessageBoxResult.No;
-                if (winnerOrTie == 1) // first player won
+                if (winnerOrTie == 1) // first player won - RED
                 {
-                    result = MessageBox.Show("Red Player Has Won! ☺\nWould you like to have another round?", "Game Over",
-                        MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
                     p1_score += 1000;
                     p2_score += p2_cellCount * 10;
                 }
-                else if (winnerOrTie == 2) // second player won
+                else if (winnerOrTie == 2) // second player won - YELLOW
                 {
-                    result = MessageBox.Show("Yellow Player Has Won! ☺\nWould you like to have another round?", "Game Over",
-                        MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
                     p2_score += 1000;
                     p1_score += p1_cellCount * 10;
                 }
                 else if (winnerOrTie == 3) // tie
                 {
-                    result = MessageBox.Show("It's a Tie! ☻\nWould you like to have another round?", "Game Over",
-                        MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
                     p1_score += p1_cellCount * 10;
                     p2_score += p2_cellCount * 10;
                 }
@@ -112,18 +114,16 @@ namespace connectFour
                 p1Score.Text = "" + p1_score;
                 p2Score.Text = "" + p2_score;
 
-                if (result == MessageBoxResult.Yes) // player want another round
+                if (winner_tie_msgBox(winnerOrTie) == MessageBoxResult.Yes) // player want another round
                     resetBoard();
                 else // player wished to exit
                     Environment.Exit(0);
             }
-            // row and col now correspond Grid's RowDefinition and ColumnDefinition mouse was 
-            // over when double clicked!
-            //}
         }
 
         private void makeAnimation(int emptyCell_row, int col)
         {
+            boardView.IsHitTestVisible = false; // disable mouse clicks again until full move and animation is over
             var img = new Image { Width = 70, Height = 70 };
             BitmapImage bitmapImage;
             String path = Environment.CurrentDirectory.Split("connectFour")[0] + "connectFour\\Resources\\"; // current project dir.
@@ -150,18 +150,25 @@ namespace connectFour
             img.RenderTransform = trans;
             int posY = emptyCell_row == 6 ? 60 : emptyCell_row == 5 ? 58 : emptyCell_row == 4 ? 56 : emptyCell_row == 3 ? 52 : emptyCell_row == 2 ? 46
                 : emptyCell_row == 1 ? 35 : 0;
-            DoubleAnimation animY = new DoubleAnimation(0, posY * (emptyCell_row + 1) - top, TimeSpan.FromMilliseconds(1500-(6-emptyCell_row)*200));
-            trans.BeginAnimation(TranslateTransform.YProperty, animY); 
 
-            // TODO: Add OnCompleteListener or some sort of checking whether the animation completed
+            DoubleAnimation animY = new DoubleAnimation(0, posY * (emptyCell_row + 1) - top, TimeSpan.FromMilliseconds(1500 - (6 - emptyCell_row) * 200));
+
+            EventHandler onComplete = (s, e) =>
+            {
+                // animY.Completed -= onComplete;
+                codeAfterAnimation(); // check for winner + update score etc
+                boardView.IsHitTestVisible = true; // enable mouse clicks again for next move
+            };
+            animY.Completed += onComplete;
+            trans.BeginAnimation(TranslateTransform.YProperty, animY);
         }
 
         private void updateBoard(int emptyCell_row, int col)
         {
-            makeAnimation(emptyCell_row, col);
-
             // update the 2D board programically
             board[emptyCell_row, col] = turn + 1;
+
+            makeAnimation(emptyCell_row, col);
         }
 
         private int findEmptyRowCell(int j)
@@ -260,9 +267,9 @@ namespace connectFour
             BitmapImage bitmapImage;
             String path = Environment.CurrentDirectory.Split("connectFour")[0] + "connectFour\\Resources\\";
 
-            if (board[i, j] == 1)
+            if (board[i, j] == 1) // RED
                 bitmapImage = new BitmapImage(new Uri(path + "predMarked.png"));
-            else // board[i,j]==2
+            else // board[i,j]==2 - YELLOW
                 bitmapImage = new BitmapImage(new Uri(path + "pyellowMarked.png"));
 
 
@@ -293,7 +300,18 @@ namespace connectFour
                 }
             }
         }
+        private MessageBoxResult winner_tie_msgBox(int p)
+        {
+            if (p == 1)
+                return MessageBox.Show("Red Player Has Won! ☺\nWould you like to have another round?", "Game Over",
+                       MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            else if (p == 2)
+                return MessageBox.Show("Yellow Player Has Won! ☺\nWould you like to have another round?", "Game Over",
+                       MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
 
+            return MessageBox.Show("It's a Tie! ☻\nWould you like to have another round?", "Game Over",
+                   MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+        }
         private void updateBonus()
         {
             //check for bonus of a player with ball in each col.
