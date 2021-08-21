98fa1c45ca810c9d5ba4cf351b9aa7d690b087ad
ShaharBlank <Shahabl@ac.sce.ac.il>
2021-08-21 18:59:07 +0300

    Online users list added
    :D

---
+++ connectFour/OnlineUsersList.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace connectFour.Resources
+{
+    /// <summary>
+    /// Interaction logic for OnlineUsersList.xaml
+    /// </summary>
+    public partial class OnlineUsersList : Window
+    {
+        public OnlineUsersList()
+        {
+            InitializeComponent();
+        }
+
+        private void userSelected(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            MessageBox.Show("You selected " + lbi.Content.ToString() + ".");
+        }
+    }
+}
