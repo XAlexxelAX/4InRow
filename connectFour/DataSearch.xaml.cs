a4af8a26ff47f70f75d359a36452fc1b48c537f5
ShaharBlank <Shahabl@ac.sce.ac.il>
2021-08-23 14:48:07 +0300

    update data search
    groundings are ready
    data is needed from server

--- connectFour/DataSearch.xaml.cs
+++ connectFour/DataSearch.xaml.cs
@@ -48,6 +48,49 @@ namespace connectFour
                     break;
 
                 case "Games: Until now":
+                    Grid DynamicGrid = new Grid();                 
+                    DynamicGrid.ShowGridLines = true;
+                    Grid.SetColumn(DynamicGrid, 1);
+                    Grid.SetRow(DynamicGrid, 4);
+                    mainGrid.Children.Add(DynamicGrid);
+
+                    RowDefinition gridRow1 = new RowDefinition();
+                    gridRow1.Height = new GridLength(50);
+                    RowDefinition gridRow2 = new RowDefinition();
+                    gridRow2.Height = new GridLength(50);
+                    DynamicGrid.RowDefinitions.Add(gridRow1);
+                    DynamicGrid.RowDefinitions.Add(gridRow2);
+
+                    ColumnDefinition gridCol1 = new ColumnDefinition();
+                    gridRow1.Height = new GridLength(50);
+                    ColumnDefinition gridCol2 = new ColumnDefinition();
+                    gridRow2.Height = new GridLength(50);
+                    DynamicGrid.ColumnDefinitions.Add(gridCol1);
+                    DynamicGrid.ColumnDefinitions.Add(gridCol2);
+
+                    TextBlock txtBlock1 = new TextBlock();
+                    txtBlock1.Text = "Test Title 1";
+                    txtBlock1.FontSize = 14;
+                    txtBlock1.FontWeight = FontWeights.Bold;
+                    txtBlock1.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlock1.VerticalAlignment = VerticalAlignment.Center;
+                    txtBlock1.HorizontalAlignment = HorizontalAlignment.Center;
+                    Grid.SetRow(txtBlock1, 0);
+                    Grid.SetColumn(txtBlock1, 0);
+                    DynamicGrid.Children.Add(txtBlock1);
+
+                    TextBlock txtBlock2 = new TextBlock();
+                    txtBlock2.Text = "Test Title 2";
+                    txtBlock2.FontSize = 14;
+                    txtBlock2.FontWeight = FontWeights.Bold;
+                    txtBlock2.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlock2.VerticalAlignment = VerticalAlignment.Top;
+                    txtBlock2.VerticalAlignment = VerticalAlignment.Center;
+                    txtBlock2.HorizontalAlignment = HorizontalAlignment.Center;
+                    Grid.SetRow(txtBlock2, 0);
+                    Grid.SetColumn(txtBlock2, 1);
+                    DynamicGrid.Children.Add(txtBlock2);
+
                     break;
 
                 case "Games: Live":
